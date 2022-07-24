76d5e1b64c90fa8dde49c1a76ecb22f31576f01a
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-07-24 11:30:35 +0500

    WIP

--- lib/my_flashlight.dart
+++ lib/my_flashlight.dart
@@ -14,40 +14,47 @@ class _MyFlashlightState extends State<MyFlashlight> {
   String usingImage = "assets/images/flashlight_off.jpg";
   // String flashlightOn = "assets/images/flashlight_on.jpg";
   // String flashlightOff = "assets/images/flashlight_off.jpg";
+  IconData flashlightSwitcher = Icons.flashlight_on;
+  String flashlightStatusMessage = "Flashlight is off";
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        // appBar: AppBar(
-        //   backgroundColor: Colors.white,
-        //   title: Text(
-        //     "My Flashlight",
-        //     style: TextStyle(color: Color(0xFF444444)),
-        //   ),
-        //   centerTitle: true,
-        // ),
+        appBar: AppBar(
+          backgroundColor: Color(0xFF000000),
+          title: Text(
+            flashlightStatusMessage,
+            style: TextStyle(color: Color(0xFFFFFFFF)),
+          ),
+          centerTitle: true,
+        ),
         body: Container(
           width: double.infinity,
           height: double.infinity,
-          color: Color(0xFF000000),
+          color: const Color(0xFF000000),
           child: Center(
             child: Image.asset(usingImage),
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          backgroundColor: Color(0xFFFFFFFF),
+          backgroundColor: const Color(0xFFFFFFFF),
           onPressed: () {
             setState(() {
               if (isFlashlightTurnOn == false) {
                 isFlashlightTurnOn = true;
+                flashlightSwitcher = Icons.flashlight_off;
+                flashlightStatusMessage = "Flashlight is on";
                 usingImage = "assets/images/flashlight_on.jpg";
               } else if (isFlashlightTurnOn == true) {
                 isFlashlightTurnOn = false;
+                flashlightSwitcher = Icons.flashlight_on;
+                flashlightStatusMessage = "Flashlight is off";
                 usingImage = "assets/images/flashlight_off.jpg";
               }
             });
           },
-          child: Icon(Icons.light, color: Color(0xFF444444)),
+          child: Icon(flashlightSwitcher, color: Color(0xFF444444)),
         ),
       ),
     );
